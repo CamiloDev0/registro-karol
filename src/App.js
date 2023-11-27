3939097851a419cdac2951477fd2d3279694cce6
Andres Gutierrez <gagutierrez454@soy.sena.edu.co>
2023-11-27 16:18:49 -0500

    Fix BUILD errors to Netlify

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
 	const [email, setEmail] = useState('');
 	const [registeredUsers, setRegisteredUsers] = useState([]);
 
-	const firebaseConfig = {
-		apiKey: 'AIzaSyDBErbEBvDu5SkAj1e7VsKENIC6of-wams',
-		authDomain: 'karlog-registro-pisina.firebaseapp.com',
-		projectId: 'karlog-registro-pisina',
-		storageBucket: 'karlog-registro-pisina.appspot.com',
-		messagingSenderId: '109347379517',
-		appId: '1:109347379517:web:f190bd2cb0d380208a4d54',
-		measurementId: 'G-EPW670CXJS',
-	};
+	// const firebaseConfig = {
+	// 	apiKey: 'AIzaSyDBErbEBvDu5SkAj1e7VsKENIC6of-wams',
+	// 	authDomain: 'karlog-registro-pisina.firebaseapp.com',
+	// 	projectId: 'karlog-registro-pisina',
+	// 	storageBucket: 'karlog-registro-pisina.appspot.com',
+	// 	messagingSenderId: '109347379517',
+	// 	appId: '1:109347379517:web:f190bd2cb0d380208a4d54',
+	// 	measurementId: 'G-EPW670CXJS',
+	// };
 
 	const getdata = async (e) => {
 		e.preventDefault();
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,4 @@
-export default{
+const objFB = {
     apiKey: "AIzaSyDBErbEBvDu5SkAj1e7VsKENIC6of-wams",
     authDomain: "karlog-registro-pisina.firebaseapp.com",
     projectId: "karlog-registro-pisina",
@@ -7,3 +7,5 @@ export default{
     appId: "1:109347379517:web:f190bd2cb0d380208a4d54",
     measurementId: "G-EPW670CXJS"
 }
+
+export default objFB;
